c7f7d438f1c3f84ef162edf8f608921a7f0c00b3
formationWeb-Lus <jiresselusa127@gmail.com>
2025-05-31 23:36:41 -0700

    Update entityModel.js

--- models/entityModel.js
+++ models/entityModel.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
 
-const entitySchema = new mongoose.Schema({
-  title: { type: String, required: true, minlength: 3 },
-  description: { type: String, required: true },
-  category: { type: String, required: true },
+const courseSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  type: { type: String, default: 'Course' },
   price: { type: Number, required: true, min: 0 },
-  quantity: { type: Number, required: true, min: 0 }
-}, { timestamps: true }); // auto-manage createdAt & updatedAt
+  duration: { type: String, required: true },
+  level: { type: String, enum: ['Beginner', 'Intermediate', 'Advanced'], required: true },
+  language: { type: String, required: true },
+  description: { type: String, required: true }
+}, { timestamps: true });
 
-module.exports = mongoose.model('Entity', entitySchema);
-
+module.exports = mongoose.model('Course', courseSchema);
