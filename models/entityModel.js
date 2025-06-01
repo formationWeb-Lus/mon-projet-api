7cd80c77029114a9bbbe01a3ee630877f3353565
formationWeb-Lus <jiresselusa127@gmail.com>
2025-05-31 23:09:09 -0700

    Update entityModel.js
    changing somethings

--- models/entityModel.js
+++ models/entityModel.js
@@ -1,13 +1,12 @@
 const mongoose = require('mongoose');
 
 const entitySchema = new mongoose.Schema({
-  title: String,
-  description: String,
-  category: String,
-  price: Number,
-  quantity: Number,
-  createdAt: { type: Date, default: Date.now },
-  updatedAt: { type: Date, default: Date.now }
-});
+  title: { type: String, required: true, minlength: 3 },
+  description: { type: String, required: true },
+  category: { type: String, required: true },
+  price: { type: Number, required: true, min: 0 },
+  quantity: { type: Number, required: true, min: 0 }
+}, { timestamps: true }); // auto-manage createdAt & updatedAt
 
 module.exports = mongoose.model('Entity', entitySchema);
+
