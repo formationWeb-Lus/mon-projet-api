42379f7b805348c14573d7a211fd14fb1576c94b
formationWeb-Lus <jiresselusa127@gmail.com>
2025-06-03 07:41:01 -0700

    Update category.js

--- models/category.js
+++ models/category.js
@@ -1,28 +1,12 @@
-// routes/categoryRoutes.js
-const express = require('express');
-const router = express.Router();
-const Category = require('../models/category'); // Assure-toi que ce fichier existe bien
+// models/category.js
+const mongoose = require('mongoose');
 
-// GET all categories
-router.get('/', async (req, res) => {
-  try {
-    const categories = await Category.find();
-    res.status(200).json(categories);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
+const categorySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  description: String
 });
 
-// POST a new category
-router.post('/', async (req, res) => {
-  try {
-    const { name, description } = req.body;
-    const category = new Category({ name, description });
-    await category.save();
-    res.status(201).json(category);
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
-});
-
-module.exports = router;
+module.exports = mongoose.model('Category', categorySchema);
